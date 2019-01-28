14faf0fcc243ec7775b76d012df590d9baf2a53c
Samuel Bartlett <sam.bartlett858@gmail.com>
2019-01-28 19:44:44 +0000

    sorted conections on /config to load the settings page

--- server/server.js
+++ server/server.js
@@ -5,16 +5,16 @@ const path = require('path');
 
 // ... profit
 
-app.use(express.static('public'));
+app.use('/', express.static('public/'));
 app.get('/updateState', function(req, res){
   console.log('request recieved');
   res.send('Hello');
   console.log('request complete');
 });
-
-app.get('/config', function(req, res){
-  res.sendfile(path.join(__dirname, '..', '/private', '/settings.html'));
-});
+app.use('/config', express.static(path.join(__dirname, '..', 'private')))
+// app.get('/config', function(req, res){
+//   res.sendFile(path.join(__dirname, '..', '/private/'));
+// });
 
 
 
