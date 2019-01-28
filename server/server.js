778709cd9a279fa8702ec2b1451e7a8c4067e944
Samuel Bartlett <sam.bartlett858@gmail.com>
2019-01-28 22:42:04 +0000

    New imports and save servre ip to file for easy access

--- server/server.js
+++ server/server.js
@@ -2,10 +2,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const ip = require('ip');
+const fs = require('fs');
 
 // ... profit
 
 app.use('/', express.static('public/'));
+
 app.get('/updateState', function(req, res){
   console.log('request recieved');
   res.send('Hello');
@@ -16,6 +19,13 @@ app.use('/config', express.static(path.join(__dirname, '..', 'private')))
 //   res.sendFile(path.join(__dirname, '..', '/private/'));
 // });
 
+function saveIP(){
+  let ipAddr = ip.address();
+  fs.writeFile(path.join(__dirname, '..', 'public', 'ip.text'), ipAddr, (err) => {
+    if (err) throw err;
+    console.log('ip saved');
+  });
+}
 
-
+saveIP();
 app.listen(8080);
