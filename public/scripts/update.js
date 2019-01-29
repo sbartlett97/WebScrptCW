3f48881c7ec049353882c7f7d14ee247dc7c9282
Samuel Bartlett <sam.bartlett858@gmail.com>
2019-01-29 15:40:13 +0000

    atempted to make dashboard auto-full screen. Works on enter key press.

--- public/scripts/update.js
+++ public/scripts/update.js
@@ -5,8 +5,33 @@ async function requestUpdateStatus(){
   const updateText = await response.text();
   console.log(updateText);
   if(updateText == 'true'){
-    window.location.reload();
+    const respone = await fetch('/');
   }
 }
 
 setInterval(requestUpdateStatus, 60000);
+
+document.addEventListener('keypress', function(e){
+  if (e.keyCode === 13) {
+    goFullScreen();
+  }
+}, false);
+
+function goFullScreen() {
+  if (!document.fullscreenElement) {
+    let fsResult = document.documentElement.requestFullscreen();
+  }
+}
+let keyPress = new KeyboardEvent('keypress', {
+  key: 'Enter',
+  code: 'Enter',
+  location: 2,
+  ctrlKey: false,
+  shiftKey: false,
+  altKey: false,
+  metaKey: false,
+  repeat: false,
+  isComposing: false,
+  keyCode: 13
+})
+document.dispatchEvent(keyPress);
