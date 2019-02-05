615c75222e46eb349c77c6b55e81aabc84aabaff
Samuel Bartlett <sam.bartlett858@gmail.com>
2019-02-05 14:36:27 +0000

    Minor refactoring of file names and locations

--- private/examples/sw/a.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", () => {
-
-  // Normally, be quiet, but, if the URL has a fragment add the music.
-  if (!!window.location.hash) {
-    const audio = new Audio('http://goo.gl/WppRG9');
-    window.logo.addEventListener( "animationstart", () => audio.play() );
-  }
-
-});
