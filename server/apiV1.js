5977eb769ffe56e99b0c0ee7ee881f2a4495742f
Samuel Bartlett <sam.bartlett858@gmail.com>
2019-02-05 20:48:05 +0000

    Split out current functionality into api file

---
+++ server/apiV1.js
@@ -0,0 +1,35 @@
+'use strict'
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+let updateState = false;
+const router = express.Router();
+router.get('/updateState', function(req, res){
+  res.responseType = 'text';
+  res.send(updateState);
+  if(updateState == 'true'){
+    updateState = 'false'
+    console.log('Dashboard Refreshed');
+  }
+});
+router.post('/update', updateDashboard);
+
+
+function updateDashboard(req, res){
+  let src = req.body.target;
+  let srcHTMLPath = path.join(__dirname, '..', 'private', 'examples', src, src + '.html');
+  let destHTMLPath = path.join(__dirname, '..', 'public', 'index.html');
+  let srcCSSPath = path.join(__dirname, '..', 'private', 'examples', src, 'style.css');
+  let destCSSPath = path.join(__dirname, '..', 'public', 'style.css');
+  fs.copyFile(srcHTMLPath, destHTMLPath, (err) => {
+    if(err) throw err;
+  });
+  fs.copyFile(srcCSSPath, destCSSPath, (err) => {
+    if(err) throw err;
+  });
+  updateState = 'true';
+  res.send('state updated');
+  console.log('Dashboard has been updated, waiting for refresh');
+}
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,41 +3,13 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+const apiV1 = require('./apiV1');
 let updateState = 'false';
 // ... profit?
 
 app.use('/', express.static('public/'));
 app.use('/config', express.static('private/'));
 app.use(express.json());
-app.get('/updateState', function(req, res){
-  res.responseType = 'text';
-  res.send(updateState);
-  if(updateState == 'true'){
-    updateState = 'false'
-  }
-});
-
-app.post('/update', function(req, res){
-  console.log('update recieved');
-  let src = req.body.target;
-  console.log('body retireved');
-  console.log('target is: '+ src);
-  let srcHTMLPath = path.join(__dirname, '..', 'private', 'examples', src, src + '.html');
-  let destHTMLPath = path.join(__dirname, '..', 'public', 'index.html');
-  console.log('Set html paths');
-  let srcCSSPath = path.join(__dirname, '..', 'private', 'examples', src, 'style.css');
-  let destCSSPath = path.join(__dirname, '..', 'public', 'style.css');
-  console.log('set css paths');
-  fs.copyFile(srcHTMLPath, destHTMLPath, (err) => {
-    if(err) throw err;
-    console.log('HTML Copied');
-  });
-  fs.copyFile(srcCSSPath, destCSSPath, (err) => {
-    if(err) throw err;
-    console.log('CSS Copied');
-  });
-  updateState = 'true';
-  res.send('state updated');
-});
+app.use(apiV1);
 
 app.listen(8080);
