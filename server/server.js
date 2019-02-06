296285d56dfdba49d9d5ec70b9f0ea47c61a2bf8
Sam <sam.bartlett858@gmail.com>
2019-02-06 11:30:15 +0000

    reorganised Server files

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const apiV1 = require('./apiV1');
+const apiV1 = require('./api/apiV1');
 // ... profit?
 
 app.use('/', express.static('webpages/public/'));
