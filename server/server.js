7be82e281d39fa0ee20b9f77d2345167cdcebd64
Sam <sam.bartlett858@gmail.com>
2019-02-07 16:23:27 +0000

    Fixed api not loading

--- server/server.js
+++ server/server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const apiV1 = require('./api/apiV1');
+const apiV1 = require('./api/api');
 // ... profit?
 
-app.use('/', express.static('webpages/public/'));
-app.use('/config', express.static('webpages/private/'));
+app.use('/', express.static('./webpages/public/'));
+app.use('/config', express.static('./webpages/private/'));
 app.use(express.json());
 app.use(apiV1);
 
