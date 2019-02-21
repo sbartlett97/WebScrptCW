2584c3fdb757c1eda171d70cf5024601c55162de
Sam <sam.bartlett858@gmail.com>
2019-02-21 09:59:40 +0000

    Bug fixes Fixed issue with stylesheets not loading correctly.
    Added conditional in ejs files so that iframes on the config page do not 
    call update script and change to match what the dashboard is displaying.
    
    Added serve-favicon to server for favicon - fixed error where dashboard 
    would request favicon from the current path and cause error

--- server/api/api.js
+++ server/api/api.js
@@ -3,29 +3,42 @@ const express = require('express');
 const router = express.Router();
 const dashboard = express.Router();
 router.post('/update', updateDashboard);
-let destPage = 'comeIn';
+router.get('/checkUpdate', checkUpdate);
+const defaultPage = 'comeIn';
+let destPage;
 
-router.get('/nextPage', sendDestination);
+
 function updateDashboard(req, res){
   console.log('update recieved');
   destPage = req.body.target;
   console.log(destPage);
-  updateRootPath();
 }
 
-function sendDestination(req, res){
-  res.send(destPage);
+async function checkUpdate(req, res){
+    res.send(destPage);
 }
 
-dashboard.get('/:dest', function(req, res){
-  const destPage = req.params['dest'];
-  res.render(`pages/${destPage}/index`);
-})
+dashboard.get('/:dest/:source?', renderFragment);
+dashboard.get('/', handleDefault);
 
+async function handleDefault(req, res){
+  res.redirect(`${defaultPage}/`);
+}
 
+async function renderFragment(req, res){
+  const source = req.params['source'];
+  const nextPage = req.params['dest'];
+  if (!nextPage){
+    res.render(`pages/${defaultPage}/index`, {
+      source: source
+    });
+  }else{
+    res.render(`pages/${nextPage}/index`, {
+      source: source
+    });
+  }
+}
 
-
-// setDeafualtRoot();
 module.exports = {
   router: router,
   dashboard: dashboard
