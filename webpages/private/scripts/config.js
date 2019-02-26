fb60caf790ea023428c0160c911c915cfbb185f7
Sam <sam.bartlett858@gmail.com>
2019-02-26 14:16:45 +0000

    General Edits -Made dashboard pages cachebale -Created shell for page to build new templates -Made config page automatically populate from contents of views/pages

--- webpages/private/scripts/config.js
+++ webpages/private/scripts/config.js
@@ -1,47 +1,79 @@
 'use strict'
+
+
+const headers = {
+  method: "POST",
+  mode: "cors",
+  cache: "no-cache",
+  headers: {
+          "Content-Type": "application/json",
+      }
+  }
+
+
 async function sendToServer(event){
   const targetPage = event.target.id;
-  console.log(targetPage);
   const data = {target: targetPage};
-  const response = await fetch('/update', {
-    method: "POST",
-    mode: "cors",
-    cache: "no-cache",
-    headers: {
-            "Content-Type": "application/json",
-        },
-    body: JSON.stringify(data)
-  });
+  headers.body = JSON.stringify(data);
+  const response = await fetch('/update', headers);
   return true;
 }
 
+
 function initialise(){
   refreshTemplates();
-
+  document.querySelector('button.refresh').addEventListener('click', refreshTemplates);
 }
 
+
+function loadPageBuilder(){
+  window.location = './builder.html';
+}
+
+
 async function refreshTemplates(){
+  const article = document.querySelector('article');
+  article.innerHTML = '';
   const response = await fetch('/refreshTemplates');
   const resText = await response.text();
-  const article = document.querySelector('article');
   JSON.parse(resText, (key, value) => {
     console.log(key);
+    const section = createSection();
     if(key != ""){
-      const section = document.createElement('section');
-      section.classList.add('iframeContainer');
-      article.appendChild(section);
       const iframe = document.createElement('iframe');
       iframe.src = `/${value}/config`
-      const selector = document.createElement('input');
-      selector.type = 'radio';
-      selector.id = `${value}`;
-      selector.name = 'template';
-      selector.classList.add('radio');
-      selector.addEventListener('click', sendToServer);
+      const radio = createRadio(value);
       section.appendChild(iframe);
-      section.appendChild(selector);
+      section.appendChild(radio);
+    }else{
+      const button = document.createElement('button');
+      section.appendChild(button);
+      const img = document.createElement('img');
+      img.src = './styles/images/add_button.png';
+      button.appendChild(img);
+      img.addEventListener('click', loadPageBuilder);
     }
   });
 }
+
+
+function createRadio(value){
+  const radio = document.createElement('input');
+  radio.type = 'radio';
+  radio.id = `${value}`;
+  radio.name = 'template';
+  radio.classList.add('radio');
+  radio.addEventListener('click', sendToServer);
+  return radio;
+}
+
+
+function createSection(){
+  const article = document.querySelector('article');
+  const section = document.createElement('section');
+  section.classList.add('iframeContainer');
+  article.appendChild(section);
+  return section;
+}
+
 document.addEventListener('DOMContentLoaded', initialise);
-document.querySelector('.button').addEventListener('click', refreshTemplates);
