3cb3f007c3068a076bd300559ae92a970c8c9988
sbartlett97 <sam.bartlett858@gmail.com>
2019-04-19 08:52:49 +0100

    Merged the text and quote page handling case Removes duplicated code and makes it easier to read through what is happening

--- server/api/dashboard.js
+++ server/api/dashboard.js
@@ -35,35 +35,47 @@ dashboard.get('/:page', function(req, res){
 
 async function loadPage(page, res){
   const con = new db(config.mysql);
-  con.query(`SELECT id FROM pages WHERE name = '${page}'`,null, {useArray: true}, function(err, row){
+  con.query(`SELECT id, type FROM pages WHERE name = '${page}'`,null, {useArray: true}, function(err, row){
     if (err)
       throw err
     let page = row[0]
     let id = page[0];
-    con.query(`SELECT value FROM content WHERE pageId = ${id}`, null, {useArray: true}, function(err, row){
-      if (err)
-        throw err
-      let textArr = row[0];
-      let text = textArr[0];
-      con.query(`SELECT element, property, value FROM styles WHERE pageId = ${id}`, {useArray : true}, function(err, rows){
+    let type = page[1];
+    if(type == "picture"){
+
+    }else{
+      con.query(`SELECT value FROM content WHERE pageId = ${id}`, null, {useArray: true}, function(err, row){
         if (err)
           throw err
-        let styleObjetcs = rows.slice(0, rows.length);
-        console.log(styleObjetcs);
-        let styleString = "";
-        styleObjetcs.forEach(function(item){
-          styleString = styleString.concat(item.element,"{ ", item.property, ": ", item.value, ";}");
-        })
-        console.log(styleString);
-        ejs.renderFile(`${pages}/text.ejs`, {text: text, source: undefined, styling: styleString}, function(err, str){
+        let textArr = row[0];
+        let by = "";
+        let text;
+        if(type == "quote"){
+          let texts = textArr[0].split("-");
+          console.log(texts);
+          text = texts[0];
+          by = by.concat("- ", texts[1]);
+        }else{
+          text = textArr[0];
+        }
+        con.query(`SELECT element, property, value FROM styles WHERE pageId = ${id}`, {useArray : true}, function(err, rows){
           if (err)
             throw err
-          res.set('Content-Type','text/html').send(str);
+          let styleObjetcs = rows.slice(0, rows.length);
+          let styleString = "";
+          styleObjetcs.forEach(function(item){
+            styleString = styleString.concat(item.element,"{ ", item.property, ": ", item.value, ";}");
+          })
+          ejs.renderFile(`${pages}/${type}.ejs`, {title: destPage, by: by, text: text, source: undefined, styling: styleString}, function(err, str){
+            if (err)
+              throw err
+            res.set('Content-Type','text/html').send(str);
+          });
         });
-      });
     });
-  });
-  con.end()
+  }
+});
+  con.end();
 }
 
 module.exports = {
