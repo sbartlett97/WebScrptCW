1f6e8f3f2406be66e5394f873c00bdefb1cd5d09
sbartlett97 <sam.bartlett858@gmail.com>
2019-04-24 18:05:47 +0100

    rearranged folder structure

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const settings = require('./api/settings');
 app.use(favicon(__dirname + '/../public/images/favicon.ico'));
 app.use('/public', express.static('public'));
 app.set('view engine', 'ejs');
-app.use('/config', express.static('./webpages/private/'));
+app.use('/config', express.static('private'));
 app.use(express.json());
 app.use(api.router);
 app.use(settings.settings);
